bf73ee09a1d7817d4a015cc5bcd943897992bab3
eximsq <eximsquare.help@gmail.com>
2023-10-12 13:17:20 +0530

    seed

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Eximsquare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.initialize_on_precompile = true
+    config.assets.serve_static_files = true
     config.assets.enabled = true
 
     # Configuration for the application, engines, and railties goes here.
